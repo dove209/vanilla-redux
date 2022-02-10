7847ade782cc11b339fa4284d4b6b11c0f10fdb6
GINI <dove209@naver.com>
2022-02-10 16:16:50 +0900

    바닐라js redux 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 
 function App() {
   return (
-    <div className="App">
-      aapp
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/:id' exact element={<Detail />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
